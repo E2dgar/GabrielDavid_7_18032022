9f949972da4386101f63cb17a91db05ca92ec31d
DG <gdavidcefim@gmail.com>
2022-03-21 10:07:54 +0100

    UI card

---
+++ src/js/Models/Recipe.js
@@ -0,0 +1,10 @@
+class Recipe {
+  constructor(data) {
+    this.id = data.id;
+    this.title = data.title;
+    this.time = data.time;
+    this.ingredients = data.ingredients;
+  }
+}
+
+export default Recipe;
---
+++ src/js/components/recipesUI.js
@@ -0,0 +1,62 @@
+import recipes from "../data/recipes";
+import Recipe from "../Models/Recipe";
+
+const recipesUI = () => {
+  const createCard = (recipe) => {
+    const article = document.createElement("article");
+
+    const img = document.createElement("div");
+    img.className = "img-container";
+
+    const title = document.createElement("h2");
+    title.textContent = recipe.name;
+
+    const recipeTime = document.createElement("span");
+    recipeTime.textContent = recipe.time + " min";
+    title.append(recipeTime);
+
+    const details = document.createElement("div");
+    details.className = "details";
+    const ingredientsWrapper = document.createElement("div");
+    ingredientsWrapper.className = "ingredients-wrapper";
+
+    recipe.ingredients.forEach((ingredient) =>
+      ingredientsWrapper.append(createIngredient(ingredient))
+    );
+
+    const description = document.createElement("div");
+    description.className = "description";
+    description.textContent = recipe.description;
+
+    details.append(ingredientsWrapper, description);
+
+    article.append(img, title, details);
+    document.querySelector(".recettes").append(article);
+  };
+
+  const createIngredient = (element) => {
+    const DOMElement = document.createElement("p");
+    DOMElement.className = "ingredient";
+
+    DOMElement.textContent = element.ingredient + ": ";
+
+    const quantity = document.createElement("span");
+    if (element.unit) {
+      let unit = "&nbsp;" + element.unit;
+      if (element.unit === "ml" || element.unit === "cl") {
+        unit = element.unit;
+      }
+
+      quantity.innerHTML = `${element.quantity + unit}`;
+    } else {
+      quantity.textContent = element.quantity;
+    }
+    DOMElement.append(quantity);
+
+    return DOMElement;
+  };
+
+  recipes.forEach((recipe) => createCard(recipe));
+};
+
+export default recipesUI;
